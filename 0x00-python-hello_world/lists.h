58a2bfa1ad9a2ae594fe839c4da558385e4707d5
AmiraAshreef <amira.m.ahmed113@gmail.com>
2023-11-27 16:08:55 -0800

    Task10

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 
 /**
@@ -6,7 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for ALX project
  */
 typedef struct listint_s
 {
