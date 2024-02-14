f9e973f88763692b10e2f5b30112a8b45ea62fb7
AmiraAshreef <amira.m.ahmed113@gmail.com>
2024-02-14 10:33:09 -0800

    mandatory

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
---- script to show the lists all privileges of the MySQL
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
